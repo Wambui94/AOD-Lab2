6e064b94a4050b8f07b32cfd1b1018f285f29625
Wambui <reniewambui15@gmail.com>
2026-04-20 23:51:55 +0200

    Lab2, edited

--- Lab2/src/aod/lab2/Tree.java
+++ Lab2/src/aod/lab2/Tree.java
@@ -1,11 +1,9 @@
-/**
- * 
- */
 package aod.lab2;
 
-/**
- * 
- */
-public interface Tree {
-
+public interface Tree<T> {
+	public void add(T item);
+	public boolean searchFor(T itemsToSearchFor);
+	public int size();
+	public void clear();
+	public boolean remove(T itemToRemove);
 }
